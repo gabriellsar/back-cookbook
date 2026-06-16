77b3fcfb771b70c819761c5bc2be25829b06b48e
gabriel <gabrielsrosas@gmail.com>
2026-06-16 19:20:10 -0300

    add serializers

---
+++ app/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from infra.models import CustomUser, Recipe, Ingredient, Step
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'role']
+        read_only_fields = ['role']
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name', 'quantity', 'is_locked']
+
+class StepSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Step
+        fields = ['id', 'order', 'instruction', 'is_locked']
+
+class RecipeSerializer(serializers.ModelSerializer):
+    # Aninhando os ingredientes e passos para leitura
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    steps = StepSerializer(many=True, read_only=True)
+    author_name = serializers.ReadOnlyField(source='author.username')
+    forked_from_title = serializers.ReadOnlyField(source='forked_from.title')
+
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'description', 'author', 'author_name', 
+            'forked_from', 'forked_from_title', 'affectionate_note',
+            'ingredients', 'steps', 'created_at', 'updated_at'
+        ]
+        read_only_fields = ['author', 'forked_from']
