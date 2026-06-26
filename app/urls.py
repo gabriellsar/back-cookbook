99dfd7854c1e37fed6c9be772ca2a79cb7684827
gabriel <gabrielsrosas@gmail.com>
2026-06-25 22:14:13 -0300

    fiz imports

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from .view.IngredientViewSet import IngredientViewSet
-from .view.RecipeViewSet import RecipeViewSet
-from .view.StepViewSet import StepViewSet
+from .views import *
 
 router = DefaultRouter()
 router.register(r'recipes', RecipeViewSet, basename='recipe')
