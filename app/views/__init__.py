0e876ff0ca5ff2bcf7ea3b68b759361989fdde43
gabriel <gabrielsrosas@gmail.com>
2026-06-25 22:09:54 -0300

    rename view to views

---
+++ app/views/__init__.py
@@ -0,0 +1,9 @@
+from .IngredientViewSet import IngredientViewSet
+from .RecipeViewSet import RecipeViewSet
+from .StepViewSet import StepViewSet
+
+__all__ = [
+    'IngredientViewSet',
+    'RecipeViewSet',
+    'StepViewSet',
+]
